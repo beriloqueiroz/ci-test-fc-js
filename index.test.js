cd64c80938712b8194b0ca981561b78f969df2fd
berilo queiroz <berilo.queiroz@gmail.com>
2025-01-15 08:37:15 -0300

    incluindo mais testes

--- index.test.js
+++ index.test.js
@@ -1,34 +1,68 @@
-const whatIsTriadAccord = require(".");
+const { identifyChord, getChord } = require(".");
 
-describe('whatIsTriadAccord', () => {
-  test('Identifica um acorde maior', () => {
-    expect(whatIsTriadAccord('C', 'E', 'G')).toBe('C');
-    expect(whatIsTriadAccord('D', 'F#', 'A')).toBe('D');
-    expect(whatIsTriadAccord('F', 'A', 'C')).toBe('F');
+describe('Chord Identification and Generation', () => {
+  describe('identifyChord function', () => {
+    it('should identify C major', () => {
+      expect(identifyChord(['C', 'E', 'G'])).toBe('C major');
+    });
+
+    it('should identify A minor', () => {
+      expect(identifyChord(['A', 'C', 'E'])).toBe('A minor');
+    });
+
+    it('should identify Ab major', () => {
+      expect(identifyChord(['Ab', 'C', 'Eb'])).toBe('G# (Ab) major');
+    });
+
+    it('should identify B diminished', () => {
+      expect(identifyChord(['B', 'D', 'F'])).toBe('B diminished');
+    });
+
+    it('should identify F augmented', () => {
+      expect(identifyChord(['F', 'A', 'C#'])).toBe('F augmented');
+    });
+
+    it('should identify G seventh', () => {
+      expect(identifyChord(['G', 'B', 'D', 'F'])).toBe('G seventh');
+    });
+
+    it('should return "Acorde não identificado" for unrecognized chord', () => {
+      expect(identifyChord(['C', 'D', 'E'])).toBe('Acorde não identificado');
+    });
   });
 
-  test('Identifica um acorde menor', () => {
-    expect(whatIsTriadAccord('C', 'D#', 'G')).toBe('Cm');
-    expect(whatIsTriadAccord('E', 'G', 'B')).toBe('Em');
-    expect(whatIsTriadAccord('D', 'F', 'A')).toBe('Dm');
-  });
+  describe('getChord function', () => {
+    it('should generate C major', () => {
+      expect(getChord('C', 'major')).toEqual(['C', 'E', 'G']);
+      expect(getChord('C')).toEqual(['C', 'E', 'G']);
+    });
 
-  test('Identifica um acorde diminuto', () => {
-    expect(whatIsTriadAccord('C', 'D#', 'F#')).toBe('Cº');
-    expect(whatIsTriadAccord('C#', 'E', 'G')).toBe('C#º');
-    expect(whatIsTriadAccord('G#', 'B', 'D')).toBe('G#º');
-  });
+    it('should generate A minor', () => {
+      expect(getChord('A', 'minor')).toEqual(['A', 'C', 'E']);
+    });
 
-  test('Substitui notas equivalentes corretamente', () => {
-    expect(whatIsTriadAccord('Db', 'F', 'Ab')).toBe('Db'); // Acorde maior
-    expect(whatIsTriadAccord('Eb', 'G', 'Bb')).toBe('Eb'); // Acorde maior
-    expect(whatIsTriadAccord('F#', 'A', 'C#')).toBe('F#m'); // Acorde menor
-    expect(whatIsTriadAccord('Ab', 'B', 'Eb')).toBe('Abm'); // Acorde menor
-  });
+    it('should generate Ab major', () => {
+      expect(getChord('Ab', 'major')).toEqual(['Ab', 'C', 'Eb']);
+    });
 
-  test('Retorna undefined para combinações inválidas', () => {
-    expect(whatIsTriadAccord('C', 'F', 'G')).toBeUndefined();
-    expect(whatIsTriadAccord('D', 'A', 'B')).toBeUndefined();
-    expect(whatIsTriadAccord('E', 'G#', 'A#')).toBeUndefined();
+    it('should generate B diminished', () => {
+      expect(getChord('B', 'diminished')).toEqual(['B', 'D', 'F']);
+    });
+
+    it('should generate F augmented', () => {
+      expect(getChord('F', 'augmented')).toEqual(['F', 'A', 'C#']);
+    });
+
+    it('should generate G seventh', () => {
+      expect(getChord('G', 'seventh')).toEqual(['G', 'B', 'D', 'F']);
+    });
+
+    it('should throw error for invalid root note', () => {
+      expect(() => getChord('H', 'major')).toThrow('Invalid root note: H');
+    });
+
+    it('should throw error for unknown chord type', () => {
+      expect(() => getChord('C', 'unknown')).toThrow('Unknown chord type: unknown');
+    });
   });
 });
